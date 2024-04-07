44b993657552b6a75befa867c4680cc69a720e05
PoroshMondal <poroshmondal@gmail.com>
2024-04-07 17:01:11 +0600

    create ToDoDatabse

---
+++ app/src/main/java/com/innovative/porosh/todoapp/db/ToDoDatabase.kt
@@ -0,0 +1,25 @@
+package com.innovative.porosh.todoapp.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.innovative.porosh.todoapp.daos.ToDoDao
+import com.innovative.porosh.todoapp.entities.ToDoModel
+
+@Database(entities = [ToDoModel::class], version = 1)
+abstract class ToDoDatabase : RoomDatabase() {
+    abstract fun getToDoDao(): ToDoDao
+
+    private var toDoDatabase: ToDoDatabase? = null
+
+    fun getDB(context: Context): ToDoDatabase {
+        return toDoDatabase ?: synchronized(this) {
+            val db = Room.databaseBuilder(context, ToDoDatabase::class.java,"ToDoDb")
+                .build()
+            toDoDatabase = db
+            db
+        }
+    }
+
+}
